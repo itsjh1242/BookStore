f969d76d47ac86ffe728f157fbd2522e3dbe8bab
itsjh <93984898+itsjh1242@users.noreply.github.com>
2022-09-14 11:37:35 +0900

    default setting
    mysql
    mysql2
    session

---
+++ bookStore/db/db.js
@@ -0,0 +1,9 @@
+const mysql2 = require("mysql2/promise");
+const pool = mysql2.createPool({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "0112",
+  database: "bookstore",
+});
+module.exports = pool;
--- bookStore/routes/index.js
+++ bookStore/routes/index.js
@@ -1,5 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const session = require("express-session");
+const pool = require("../db/db");
+const sessionStore = require("../db/session");
+const router = express.Router();
+
+/* 세션 보안 */
+router.use(
+  session({
+      secret: "bookstore",
+      resave: false,
+      saveUninitialized: true,
+      store: sessionStore,
+  })
+);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
