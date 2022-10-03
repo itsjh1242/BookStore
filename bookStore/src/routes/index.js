43dec39c74c7a84c251c021f7367414a845a0deb
itsjh <93984898+itsjh1242@users.noreply.github.com>
2022-10-03 19:19:52 +0900

    u

--- bookStore/src/routes/index.js
+++ bookStore/src/routes/index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const session = require("express-session");
-const pool = require("../db/db");
-const sessionStore = require("../db/session");
+var MySQLStore = require('express-mysql-session');
+const pool = require("../../db/db");
+const sessionStore = require("../../db/session");
 const router = express.Router();
 
 /* 세션 보안 */
+var options={
+  host:'localhost',
+  user: 'root',
+  password: 'itsjh0112',
+  port: 3306,
+  database: 'bookstore'
+};
+
+var sessionStore = new MySQLStore(options);
+
 router.use(
   session({
-      secret: "bookstore",
+      secret: 'asdqweasdasd',
       resave: false,
       saveUninitialized: true,
-      store: sessionStore,
+      store: sessionStore
   })
 );
 
